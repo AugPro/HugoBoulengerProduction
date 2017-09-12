b5243419fdc9fd8107f1197afcfc6cb87fb274c5
Augustin <augustin.pro@gmail.com>
2017-09-12 16:32:12 +0200

    portfolio app creation

--- HugoBoulengerProduction/urls.py
+++ HugoBoulengerProduction/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^about/bio/', include('bio.urls')),
+    url(r'^Photos/', include('portfolios.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
