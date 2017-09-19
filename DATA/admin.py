2f9190acf8ffc4f43a895843b7811a8e8c9986a5
borisghidaglia <borisghidaglia@gmail.com>
2017-09-19 13:19:32 +0200

    DATA basic OK ( amended : no media )

---
+++ DATA/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from . import models
+from django.utils.html import format_html
+# Register your models here.
+
+class PhotoAdmin(admin.ModelAdmin):
+    """docstring forPhotosAdmin."""
+    date_hierarchy = 'date'
+    exclude = ['key']
+    def image_tag(self, photo):
+        return format_html('<img src="{}" style="max-width:30%;max-height:30%"/>'.format(photo.ld.url))
+    image_tag.short_description = 'Photo'
+    def tags_tag(self, photo):
+        foo = ''
+        for tag in photo.tags.all():
+            foo += str(tag)+', '
+        return foo
+    tags_tag.short_description = 'Tags'
+    list_display= ['image_tag','title','date','tags_tag','key']
+admin.site.register(models.Photo, PhotoAdmin)
+admin.site.register(models.Tag)
---
+++ DATA/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils.crypto import get_random_string
+from unidecode import unidecode
+
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=30,unique=True)
+    def __str__(self):
+        return self.tag
+    def save(self, *args, **kwargs):
+        self.tag = unidecode(self.tag.lower())
+        super().save(*args, **kwargs)
+
+class Photo(models.Model):
+    """docstring for Photo."""
+    hd = models.ImageField(upload_to='DATA/hd/', verbose_name='Photo haute définition')
+    ld = models.ImageField(upload_to='DATA/ld/', verbose_name='Photo basse définition')
+    key = models.CharField(max_length=16, blank=True, unique=True)
+    title = models.CharField(max_length=40, default='')
+    tags = models.ManyToManyField(Tag)
+    date = models.DateField(auto_now_add=True)
+    def __str__(self):
+        return self.hd.name
+    def save(self, *args, **kwargs):
+        # attrib random key
+        self.key = get_random_string(length=16)
+        while Photo.objects.filter(key=self.key).exists():
+            self.key = get_random_string(length=16)
+        super().save(*args, **kwargs)
