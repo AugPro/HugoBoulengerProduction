becab88e8130053ad82a4df9472164db5b1e6cde
Augustin <augustin.pro@gmail.com>
2017-09-26 17:20:55 +0200

    Subscribe V2

--- subscribe/models.py
+++ subscribe/models.py
@@ -8,3 +8,9 @@ class Subscriber(models.Model):
     first_name = models.CharField(max_length=30)
     def __str__(self):
         return self.email
+
+class Text(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    def __str__(self):
+        return self.text[:30]
--- subscribe/views.py
+++ subscribe/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
-from . import forms
+from django.shortcuts import render,redirect,get_list_or_404
+from . import forms,models
 # Create your views here.
 
 def home(request):
-    saved=False
-    validate = "Bravo"
     form = forms.SubcriberForm(request.POST or None)
     if form.is_valid():
         form.save()
-        saved= True
+        return redirect('main_home')
+    text = get_list_or_404(models.Text)[0]
     return render(request, 'subscribe/subscribe.html', locals())
